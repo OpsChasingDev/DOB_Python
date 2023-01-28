0569a8a4a0049e9ec2448a2d2547bfcee122578b
Robert Stapleton <robert@stapleton-family.net>
2023-01-28 09:14:50 -0500

    create new vpc and store return values

--- aws/main.py
+++ aws/main.py
@@ -1,6 +1,10 @@
 import boto3
 
 aws_client = boto3.client("ec2", region_name="eu-central-1")
+aws_resource = boto3.resource("ec2")
+
+new_vpc = aws_resource.create_vpc(CidrBlock="10.0.0.0/16")
+
 vpcs = aws_client.describe_vpcs()
 vpcs = vpcs['Vpcs']
 
