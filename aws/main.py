29653eac18436d5195a3b6d0fb132d3217dfb9fa
Robert Stapleton <robert@stapleton-family.net>
2023-01-28 09:19:39 -0500

    create subnets on the new vpc and tags

--- aws/main.py
+++ aws/main.py
@@ -3,7 +3,23 @@ import boto3
 aws_client = boto3.client("ec2", region_name="eu-central-1")
 aws_resource = boto3.resource("ec2")
 
-new_vpc = aws_resource.create_vpc(CidrBlock="10.0.0.0/16")
+new_vpc = aws_resource.create_vpc(
+    CidrBlock="10.0.0.0/16"
+)
+new_vpc.create_subnet(
+    CidrBlock="10.0.1.0/24"
+)
+new_vpc.create_subnet(
+    CidrBlock="10.0.2.0/24"
+)
+new_vpc.create_tags(
+    Tags=[
+        {
+            'Key': 'Name',
+            'Value': 'new-vpc'
+        }
+    ]
+)
 
 vpcs = aws_client.describe_vpcs()
 vpcs = vpcs['Vpcs']
