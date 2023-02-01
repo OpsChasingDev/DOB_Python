095fe13404694c6d48cfcca500e4bcab5d144b11
Robert Stapleton <robert@stapleton-family.net>
2023-01-31 19:05:22 -0500

    returning health check for all instances

--- aws_ec2_status_check/main.py
+++ aws_ec2_status_check/main.py
@@ -2,3 +2,10 @@ import boto3
 
 aws_client = boto3.client("ec2", region_name="us-east-1")
 aws_resource = boto3.resource("ec2", region_name="us-east-1")
+
+# returns state of all ec2 instances
+instances = aws_client.describe_instances()
+for reservation in instances["Reservations"]:
+    server = reservation["Instances"]
+    for s in server:
+        print(f"Instance {s['InstanceId']} is {s['State']['Name']}")
