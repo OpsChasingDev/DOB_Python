4cb194417d2c11a57fc09779880c454693474057
Robert Stapleton <robert@stapleton-family.net>
2023-02-01 18:03:04 -0500

    added in cluster version info

--- eks_cluster_info/main.py
+++ eks_cluster_info/main.py
@@ -8,6 +8,9 @@ for clust in clusters:
     cluster_response = eks_client.describe_cluster(
         name=clust
     )
-    cluster_status = cluster_response['cluster']['status']
-    print(cluster_status)
+    cluster_info = cluster_response['cluster']
+    cluster_status = cluster_info['status']
+    cluster_endpoint = cluster_info['endpoint']
+    cluster_version = cluster_info['version']
+    print(f"Status: {cluster_status} | Endpoint: {cluster_endpoint} | Version: {cluster_version}")
     
