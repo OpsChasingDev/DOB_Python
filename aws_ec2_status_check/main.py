d691c63a14b41597156e8ce655ce1b45d380df67
Robert Stapleton <robert@stapleton-family.net>
2023-01-31 19:12:14 -0500

    gets system status information of all ec2 instances

--- aws_ec2_status_check/main.py
+++ aws_ec2_status_check/main.py
@@ -9,3 +9,10 @@ for reservation in instances["Reservations"]:
     server = reservation["Instances"]
     for s in server:
         print(f"Instance {s['InstanceId']} is {s['State']['Name']}")
+
+# return health check of all ec2 instances
+statuses = aws_client.describe_instance_status()
+for status in statuses["InstanceStatuses"]:
+    instance_status = status["InstanceStatus"]["Status"]
+    system_status = status["SystemStatus"]["Status"]
+    print(f"Instance {status['InstanceId']} status is {instance_status} and system is {system_status}") 
