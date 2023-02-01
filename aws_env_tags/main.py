c2b96b850c4c829894aa2b4c98f43315c5aa2f3e
Robert Stapleton <robert@stapleton-family.net>
2023-02-01 17:39:50 -0500

    create tags for germany region as development

--- aws_env_tags/main.py
+++ aws_env_tags/main.py
@@ -3,8 +3,13 @@ import boto3
 aws_client_paris = boto3.client("ec2", region_name="eu-west-3")
 aws_resource_paris = boto3.resource("ec2", region_name="eu-west-3")
 
-ids_paris = []
+aws_client_germany = boto3.client("ec2", region_name="eu-central-1")
+aws_resource_germany = boto3.resource("ec2", region_name="eu-central-1")
 
+ids_paris = []
+ids_germany = []
+
+# set tags for Paris region instances
 reservations_paris = aws_client_paris.describe_instances()["Reservations"]
 for instance in reservations_paris:
     instances = instance["Instances"]
@@ -17,6 +22,23 @@ aws_resource_paris.create_tags(
         {
             'Key': 'Environment',
             'Value': 'production'
+        },
+    ]
+)
+
+# set tags for Germany region instances
+reservations_germany = aws_client_germany.describe_instances()["Reservations"]
+for instance in reservations_germany:
+    instances = instance["Instances"]
+    for instance in instances:
+        ids_germany.append(instance["InstanceId"])
+
+aws_resource_germany.create_tags(
+    Resources=ids_germany,
+    Tags=[
+        {
+            'Key': 'Environment',
+            'Value': 'development'
         },
     ]
 )
