53cf3562b0aaabeb5f771682697d0d2895778bc8
Robert Stapleton <robert@stapleton-family.net>
2023-01-31 19:15:13 -0500

    consolidated state function into status function

--- aws_ec2_status_check/main.py
+++ aws_ec2_status_check/main.py
@@ -3,16 +3,10 @@ import boto3
 aws_client = boto3.client("ec2", region_name="us-east-1")
 aws_resource = boto3.resource("ec2", region_name="us-east-1")
 
-# returns state of all ec2 instances
-instances = aws_client.describe_instances()
-for reservation in instances["Reservations"]:
-    server = reservation["Instances"]
-    for s in server:
-        print(f"Instance {s['InstanceId']} is {s['State']['Name']}")
-
 # return health check of all ec2 instances
 statuses = aws_client.describe_instance_status()
 for status in statuses["InstanceStatuses"]:
     instance_status = status["InstanceStatus"]["Status"]
     system_status = status["SystemStatus"]["Status"]
-    print(f"Instance {status['InstanceId']} status is {instance_status} and system is {system_status}") 
+    state = status["InstanceState"]["Name"]
+    print(f"Instance: {status['InstanceId']} | Status: {instance_status} | System: {system_status} | State: {state}") 
