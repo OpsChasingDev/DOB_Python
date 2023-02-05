46844a0ad712d034a79af7aa5d780756b3cf7411
Robert Stapleton <robert@stapleton-family.net>
2023-02-05 08:03:41 -0500

    snapshot creation for all volumes

--- aws_ec2_snapshots/main.py
+++ aws_ec2_snapshots/main.py
@@ -3,3 +3,10 @@ import boto3
 aws_client = boto3.client("ec2", region_name="us-east-1")
 aws_resource = boto3.resource("ec2", region_name="us-east-1")
 
+volumes = aws_client.describe_volumes()
+
+for volume in volumes['Volumes']:
+    snapshot = aws_client.create_snapshot(
+        VolumeId=volume['VolumeId']
+    )
+    print(snapshot)
