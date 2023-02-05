d4e8729ba183440dab28f248fbd5bae969387182
Robert Stapleton <robert@stapleton-family.net>
2023-02-05 08:12:22 -0500

    snapshots only for prod

--- aws_ec2_snapshots/main.py
+++ aws_ec2_snapshots/main.py
@@ -5,7 +5,14 @@ aws_client = boto3.client("ec2", region_name="us-east-1")
 aws_resource = boto3.resource("ec2", region_name="us-east-1")
 
 def create_snapshot():
-    volumes = aws_client.describe_volumes()
+    volumes = aws_client.describe_volumes(
+        Filters=[
+            {
+                'Name': 'tag:Name',
+                'Value': ['prod']
+            }
+        ]
+    )
 
     for volume in volumes['Volumes']:
         snapshot = aws_client.create_snapshot(
@@ -13,7 +20,7 @@ def create_snapshot():
         )
         print(snapshot)
 
-schedule.every().day.do(create_snapshot)
+schedule.every(15).seconds.do(create_snapshot)
 
 while True:
     schedule.run_pending()
