ce0fef6badef1bda5293e0cb0ff0ae3f0838a29d
Robert Stapleton <robert@stapleton-family.net>
2023-02-05 09:01:32 -0500

    creating new volume from snapshot and attaching to ec2 instance

--- aws_ec2_snapshots/restore.py
+++ aws_ec2_snapshots/restore.py
@@ -1,3 +1,5 @@
+# got some hard coded stuff in here - caution
+
 import boto3
 from operator import itemgetter
 
@@ -6,6 +8,7 @@ aws_resource = boto3.resource("ec2", region_name="us-east-1")
 
 instance_id = "i-05d9202e693a8893f"
 
+# get volumes for prod ec2 instance
 volumes = aws_client.describe_volumes(
     Filters=[
         {
@@ -16,4 +19,39 @@ volumes = aws_client.describe_volumes(
 )
 
 ec2_volume = volumes['Volumes'][0]
-print(ec2_volume)
+
+# get latest snapshot for desired volume
+snapshots = aws_client.describe_snapshots(
+    OwnerIds=['self'],
+    Filters=[
+        {
+            'Name': 'volume-id',
+            'Values': [ec2_volume['VolumeId']]
+        }
+    ]
+)
+latest_snapshot = sorted(snapshots['Snapshots'], key=itemgetter('StartTime'), reverse=True)[0]
+
+# create the new volume from the snapshot
+new_volume = aws_client.create_volume(
+    SnapshotId=latest_snapshot['SnapshotId'],
+    AvailabilityZone="us-east-1d",
+    TagSpecifications=[
+        {
+            'ResourceType': 'volume',
+            'Tags': [
+                {
+                    'Key': 'Name',
+                    'Value': 'prod'
+                }
+            ]
+        }
+    ]
+)
+
+# attach newly created volume to ec2 instance
+aws_resource.Instance(instance_id).attach_volume(
+    VolumeId=new_volume['VolumeId'],
+    # the device has the last character manually modified because we cannot attach the new volume on the same device already added to the ec2 instance
+    Device='/dev/xvdb'
+)
