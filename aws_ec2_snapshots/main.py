2887605227d529d0ab9ec98bfd70135a67e2bf9b
Robert Stapleton <robert@stapleton-family.net>
2023-02-05 08:06:11 -0500

    implemented scheduler

--- aws_ec2_snapshots/main.py
+++ aws_ec2_snapshots/main.py
@@ -1,12 +1,19 @@
 import boto3
+import schedule
 
 aws_client = boto3.client("ec2", region_name="us-east-1")
 aws_resource = boto3.resource("ec2", region_name="us-east-1")
 
-volumes = aws_client.describe_volumes()
+def create_snapshot():
+    volumes = aws_client.describe_volumes()
 
-for volume in volumes['Volumes']:
-    snapshot = aws_client.create_snapshot(
-        VolumeId=volume['VolumeId']
-    )
-    print(snapshot)
+    for volume in volumes['Volumes']:
+        snapshot = aws_client.create_snapshot(
+            VolumeId=volume['VolumeId']
+        )
+        print(snapshot)
+
+schedule.every().day.do(create_snapshot)
+
+while True:
+    schedule.run_pending()
