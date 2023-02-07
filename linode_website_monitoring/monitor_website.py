f2f9e6f34665fe288abec577123c3d8d2159c895
Robert Stapleton <robert@stapleton-family.net>
2023-02-07 09:46:33 -0500

    logic for website health check using response 200

--- linode_website_monitoring/monitor_website.py
+++ linode_website_monitoring/monitor_website.py
@@ -1,4 +1,7 @@
 import requests
 
 response = requests.get('http://143.42.119.228:8080/')
-print(response)
+if response.status_code == 200:
+    print('Site is ok')
+else:
+    print('Website is down!!!')
