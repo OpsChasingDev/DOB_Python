7c9b0f14df3f6680ec7775ed287d0c21efb1ddce
Robert Stapleton <robert@stapleton-family.net>
2023-02-07 10:22:26 -0500

    stuck on auth - M365 app password instructions from all sources not working right

--- linode_website_monitoring/monitor_website.py
+++ linode_website_monitoring/monitor_website.py
@@ -1,7 +1,13 @@
 import requests
+import smtplib
 
 response = requests.get('http://143.42.119.228:8080/')
 if response.status_code == 200:
     print('Site is ok')
 else:
     print('Website is down!!!')
+    # send email notification
+    with smtplib.SMTP('stapletonfamily-net02c.mail.protection.outlook.com', 25) as email:
+        email.starttls()
+        email.ehlo()
+        email.login("robert@stapleton-family.net", "")
