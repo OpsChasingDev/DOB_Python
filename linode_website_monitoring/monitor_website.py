d4ce1c27a56445e6f9f438ec8234ff815a6a7a98
Robert Stapleton <robert@stapleton-family.net>
2023-02-07 18:24:11 -0500

    restarts app after server is rebooted

--- linode_website_monitoring/monitor_website.py
+++ linode_website_monitoring/monitor_website.py
@@ -14,12 +14,28 @@ EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 LINODE_TOKEN = os.environ.get('LINODE_TOKEN')
 
 def send_email(email_message):
+    print("Sending email...")
     with smtplib.SMTP('stapletonfamily-net02c.mail.protection.outlook.com', 25) as email:
         email.starttls()
         email.ehlo()
         email.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
         message = f"Website is down!!!\n{email_message}"
         email.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, message)
+
+def restart_app():
+    print("Restarting application...")
+    ssh = paramiko.SSHClient()
+    # this line avoids the prompt during first SSH connection
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(
+        hostname=public_ip,
+        username='root',
+        key_filename='C:\\Users\\Robert\\.ssh\\id_rsa')
+    stdin, stdout, stderr = ssh.exec_command(f'docker start {docker_container_id}')
+    print(stdout.readlines())
+    ssh.close()
+    print("Application restarted")
+
 
 # checks to see if a response is given from the server at all
 try:
@@ -33,22 +49,17 @@ try:
         #send_email(f"Returned http status {response.status_code}.")
 
         # restart app
-        ssh = paramiko.SSHClient()
-        # this line avoids the prompt during first SSH connection
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(
-            hostname=public_ip,
-            username='root',
-            key_filename='C:\\Users\\Robert\\.ssh\\id_rsa')
-        stdin, stdout, stderr = ssh.exec_command(f'docker start {docker_container_id}')
-        print(stdout.readlines())
-        ssh.close()
-        print("Application restarted")
+        restart_app()
+
 except Exception as ex:
     print(f'exception: {ex}')
     #send_email(f"Connection timed out with the below exception:\n{ex}")
 
     # restart server using linode library
+    print('Rebooting server...')
     client = linode_api4.LinodeClient(LINODE_TOKEN)
     nginx_server = client.load(linode_api4.Instance, linode_id)
     nginx_server.reboot()
+
+    # restart app
+    restart_app()
