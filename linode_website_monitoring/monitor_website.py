67b39aee0e194164ca0610722d5b8835eef52e36
Robert Stapleton <robert@stapleton-family.net>
2023-02-07 18:36:15 -0500

    changed order of function definitions and cleaned up

--- linode_website_monitoring/monitor_website.py
+++ linode_website_monitoring/monitor_website.py
@@ -14,17 +14,8 @@ EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 LINODE_TOKEN = os.environ.get('LINODE_TOKEN')
 
-def send_email(email_message):
-    print("Sending email...")
-    with smtplib.SMTP('stapletonfamily-net02c.mail.protection.outlook.com', 25) as email:
-        email.starttls()
-        email.ehlo()
-        email.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-        message = f"Website is down!!!\n{email_message}"
-        email.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, message)
-
 def restart_app():
-    print("Restarting application...")
+    print("Restarting app...")
     ssh = paramiko.SSHClient()
     # this line avoids the prompt during first SSH connection
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -35,7 +26,31 @@ def restart_app():
     stdin, stdout, stderr = ssh.exec_command(f'docker start {docker_container_id}')
     print(stdout.readlines())
     ssh.close()
-    print("Application restarted")
+    print("App restarted")
+
+def restart_server_and_app():
+    # restarts server
+    print('Rebooting server and starting app...')
+    client = linode_api4.LinodeClient(LINODE_TOKEN)
+    nginx_server = client.load(linode_api4.Instance, linode_id)
+    nginx_server.reboot()
+
+    # restarts app container
+    while True:
+        nginx_server = client.load(linode_api4.Instance, linode_id)
+        if nginx_server.status == 'running':
+            time.sleep(5)
+            restart_app()
+            break
+
+def send_email(email_message):
+    print("Sending email...")
+    with smtplib.SMTP('stapletonfamily-net02c.mail.protection.outlook.com', 25) as email:
+        email.starttls()
+        email.ehlo()
+        email.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        message = f"Website is down!!!\n{email_message}"
+        email.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, message)
 
 
 # checks to see if a response is given from the server at all
@@ -46,26 +61,9 @@ try:
         print('Site is ok')
     else:
         print('Website is down!!!')
-        # send email notification
         #send_email(f"Returned http status {response.status_code}.")
-
-        # restart app
         restart_app()
-
 except Exception as ex:
     print(f'exception: {ex}')
     #send_email(f"Connection timed out with the below exception:\n{ex}")
-
-    # restart server using linode library
-    print('Rebooting server...')
-    client = linode_api4.LinodeClient(LINODE_TOKEN)
-    nginx_server = client.load(linode_api4.Instance, linode_id)
-    nginx_server.reboot()
-
-    # restart app
-    while True:
-        nginx_server = client.load(linode_api4.Instance, linode_id)
-        if nginx_server.status == 'running':
-            time.sleep(5)
-            restart_app()
-            break
+    restart_server_and_app()
